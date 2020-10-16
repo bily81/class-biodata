8f3d2fa7090ee7056a461fee4b420227e03c537c
bily81 <72961580+bily81@users.noreply.github.com>
2020-10-16 18:23:50 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hp Pavillion 14
+ */
+public class Biodata {
+    
+     public void infoNama(){
+        System.out.println("Nama : Muhammad Sabilillah Syahbana");
+    }
+     
+      public void infoAlamat(){
+        System.out.println("Alamat : Dsn. Kucur Ds. Sumberejo Kec. Purwosari");
+    }
+      
+       public void infoTtl(){
+        System.out.println("TTL : 1 September, 2004");
+    }
+       
+       public void infoKelas(){
+        System.out.println("Kelas : XI-RPL 1");
+    }
+       
+       public void infoHobby(){
+        System.out.println("Hobby : Rebahan");
+    }
+}
---
+++ Utama.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hp Pavillion 14
+ */
+public class Utama {
+    public static void main(String [] args){
+        Biodata biodata = new Biodata();
+        biodata.infoNama();
+        biodata.infoAlamat();
+        biodata.infoTtl();
+        biodata.infoKelas();
+        biodata.infoHobby();
+    }
+}
